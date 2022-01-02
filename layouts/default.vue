04a66c024bcd841a74ab2d4f36c38d3caac7f661
Nicolas <nicolas.vieras@usach.cl>
2022-01-02 01:38:22 -0300

    Propuesta Frontend TBD

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <Navigation/>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+import Navigation from "../components/Navigation.vue";
+
+export default {
+    components: {
+        Navigation
+    }
+};
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+    color: whitesmoke;
+
+}
+
+    body {
+        background: #2d3436;
+        color:whitesmoke;
+        font-family: Arial, Helvetica, sans-serif;
+        padding-top: 0;
+    }
+
+    .content {  
+        padding: 2rem;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Aplicación de prueba para laboratorio</h1>
+    </div>
+  
+</template>
+
+<script>
+import Navigation from "../components/Navigation.vue";
+
+export default {
+    components: {
+        Navigation
+    },
+    name: "IndexPage",
+    head() {
+        return {
+            title: "Gestión de Voluntariado | Home",
+        };
+    }
+   
+};
+</script>
